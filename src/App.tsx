5293f61c28598d840ac05261a41bc85429211d6e
creator2104 <vinitr.prajapati2104@gmail.com>
2025-06-25 15:53:50 +0530

    initalized a new project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+      <h1 className='p-4 bg-black text-white'>
+        Hello, Vite + React + TypeScript + Tailwind CSS!
+      </h1>
+  )
+}
+
+export default App
